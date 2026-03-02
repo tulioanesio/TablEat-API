63ecc633e622f6bb3035bef1bc46f98ba1ebccb5
tulioanesio <tulioluv@gmail.com>
2026-03-02 16:27:17 -0300

    feat: adds swagger and order base files

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { PrismaModule } from './prisma/prisma.module';
 import { ProductModule } from './product/product.module';
 import { CategoryModule } from './category/category.module';
 import { MenuModule } from './menu/menu.module';
+import { OrderModule } from './order/order.module';
 
 @Module({
-  imports: [PrismaModule, ProductModule, CategoryModule, MenuModule],
+  imports: [PrismaModule, ProductModule, CategoryModule, MenuModule, OrderModule],
   controllers: [],
   providers: [PrismaService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('TablEat API')
+    .setDescription('API to introduces a new way to order food in restaurants')
+    .setVersion('1.0')
+    .addTag('tablEat')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
