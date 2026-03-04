75fb6107f41d77a9d05bf84eff86dfe0e2d7586f
tulioanesio <tulioluv@gmail.com>
2026-03-03 22:26:26 -0300

    fix: adds error handling to table service

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,6 +1,12 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
 import { UpdateTableDto } from './dto/update-table.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from 'src/generated/prisma/client';
 
 @Injectable()
 export class TableService implements OnModuleInit {
@@ -11,7 +17,7 @@ export class TableService implements OnModuleInit {
   }
 
   private async ensureFixedTableExists() {
-    const fixedId = "01955e9c-8a20-72d5-9988-123456789abc";
+    const fixedId = '01955e9c-8a20-72d5-9988-123456789abc';
 
     await this.prismaService.table.upsert({
       where: { id: fixedId },
@@ -24,13 +30,31 @@ export class TableService implements OnModuleInit {
   }
 
   async findAll() {
-    return await this.prismaService.table.findMany();
+    try {
+      return await this.prismaService.table.findMany();
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Internal server error while fetching tables',
+      );
+    }
   }
 
   async update(id: string, updateTableDto: UpdateTableDto) {
-    return await this.prismaService.table.update({
-      where: { id },
-      data: updateTableDto,
-    });
+    try {
+      return await this.prismaService.table.update({
+        where: { id },
+        data: updateTableDto,
+      });
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2025'
+      ) {
+        throw new NotFoundException('Table not found');
+      }
+      throw new InternalServerErrorException(
+        'Internal server error while updating table',
+      );
+    }
   }
 }
